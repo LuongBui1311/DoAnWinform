011aca23bdacc053b4778a4ac3ebfbecd35e77c3
Luong Bui <luonglb1311@gmail.com>
2023-03-14 08:00:34 +0700

    first commit

---
+++ Login/SplashScreen.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Threading;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for SplashSceen.xaml
+    /// </summary>
+    public partial class SplashSceen : Window
+    {
+        public SplashSceen()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress= true;
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value= e.ProgressPercentage;
+            if (progressBar.Value == 100)
+            {
+                LogIn logIn = new LogIn();
+                Close();
+                logIn.ShowDialog();
+            }
+        }
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(80);
+            }
+        }
+    }
+}
